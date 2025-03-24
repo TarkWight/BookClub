6c9aeb15528149e6e5ab34f6cc571670f66c767a
TarkWight <arsentiy.gonda@gmail.com>
2025-03-24 16:19:07 +0700

    Setup environment + preprocessing service
    -- chunk manager is passed as environment
    -- preprocessing is registered in init

--- BookClub/Application/BookClubApp.swift
+++ BookClub/Application/BookClubApp.swift
@@ -9,9 +9,34 @@ import SwiftUI
 
 @main
 struct BookClubApp: App {
+    @StateObject private var textManager = TextChunkManager()
+    
+    //TODO: - Вызывается один раз, чтобы загрузить ан устройство книгу
+    /// Иначе нужно закомментировать весь init
+    init() {
+        if let plistURL = Bundle.main.url(forResource: "LocalConfig", withExtension: "plist"),
+           let data = try? Data(contentsOf: plistURL),
+           let dict = try? PropertyListSerialization.propertyList(from: data, format: nil) as? [String: Any],
+           let path = dict["BookFilePath"] as? String {
+            
+            let fileURL = URL(fileURLWithPath: path)
+            let processor = TextProcessingService()
+            
+            do {
+                try processor.processRawText(from: fileURL)
+                print("Всё готово.")
+            } catch {
+                print("Ошибка при обработке текста: \(error)")
+            }
+        } else {
+            print("LocalConfig.plist не найден или повреждён")
+        }
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(textManager)
         }
     }
 }
