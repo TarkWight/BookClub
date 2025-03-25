0139a67d5745399bf179c0c980bca14b883a9b6c
TarkWight <arsentiy.gonda@gmail.com>
2025-03-25 12:52:24 +0700

    Refactoring tab bar view
    -- the body is clear and clean
    -- the entire layout is broken down into small variables and functions
    -- the structure strictly separates the business logic (callbacks) from the UI

--- BookClub/Presentation/Scenes/TabBar/TabBarView.swift
+++ BookClub/Presentation/Scenes/TabBar/TabBarView.swift
@@ -15,74 +15,80 @@ struct TabBarView: View {
 
     var body: some View {
         ZStack(alignment: .bottom) {
-            HStack {
-                HStack(spacing: 0) {
-                    tabButton(.library)
-                    tabButton(.search)
-                }
-
-                Spacer()
-
-                HStack(spacing: 0) {
-                    tabButton(.bookmarks)
-                    logoutButton()
-                }
-            }
-            .frame(height: Constants.tabBarHeight)
-            .padding(.horizontal, Constants.horizontalPadding)
-            .background(AppColors.accentDark)
-            .clipShape(Capsule())
-
-            Button(action: {
-                onReadSelected()
-            }, label: {
-                AppImages.play
-                    .resizable()
-                    .frame(width: Constants.iconSize, height: Constants.iconSize)
-                    .foregroundColor(AppColors.accentDark)
-                    .frame(width: Constants.playButtonSize, height: Constants.playButtonSize)
-                    .background(AppColors.secondary)
-                    .clipShape(Circle())
-            })
-            .position(x: (UIScreen.main.bounds.width - Constants.horizontalPadding * 2) / 2,
-                      y: Constants.playButtonOffsetY)
+            tabsContainer
+            centerPlayButton
         }
         .frame(height: Constants.tabBarTotalHeight)
+    }
+}
 
+// MARK: - UI Components
+
+private extension TabBarView {
+    var tabsContainer: some View {
+        HStack {
+            leadingTabs
+            Spacer()
+            trailingTabs
+        }
+        .frame(height: Constants.tabBarHeight)
+        .padding(.horizontal, Constants.horizontalPadding)
+        .background(Constants.tabBarBackgroundColor)
+        .clipShape(Capsule())
     }
 
-    @ViewBuilder
-    private func tabButton(_ tab: Tab) -> some View {
+    var leadingTabs: some View {
+        HStack(spacing: 0) {
+            tabButton(.library)
+            tabButton(.search)
+        }
+    }
+
+    var trailingTabs: some View {
+        HStack(spacing: 0) {
+            tabButton(.bookmarks)
+            logoutButton
+        }
+    }
+
+    var centerPlayButton: some View {
+        Button(action: onReadSelected) {
+            AppImages.play
+                .resizable()
+                .frame(width: Constants.iconSize, height: Constants.iconSize)
+                .foregroundColor(Constants.playButtonIconColor)
+                .frame(width: Constants.playButtonSize, height: Constants.playButtonSize)
+                .background(Constants.playButtonBackgroundColor)
+                .clipShape(Circle())
+        }
+        .position(x: (UIScreen.main.bounds.width - Constants.horizontalPadding * 2) / 2,
+                  y: Constants.playButtonOffsetY)
+    }
+
+    func tabButton(_ tab: Tab) -> some View {
         Button(action: {
             onTabSelected(tab)
         }, label: {
-            VStack {
-                tab.icon
-                    .resizable()
-                    .renderingMode(.template)
-                    .frame(width: Constants.iconSize, height: Constants.iconSize)
-                    .foregroundColor(selectedTab == tab
-                                     ? AppColors.white
-                                     : AppColors.accentMedium)
-            }
-            .frame(width: Constants.tabSize, height: Constants.tabSize)
+            tab.icon
+                .resizable()
+                .renderingMode(.template)
+                .frame(width: Constants.iconSize, height: Constants.iconSize)
+                .foregroundColor(selectedTab == tab
+                                 ? Constants.selectedTabColor
+                                 : Constants.unselectedTabColor)
+                .frame(width: Constants.tabSize, height: Constants.tabSize)
         })
     }
 
-    @ViewBuilder
-    private func logoutButton() -> some View {
-        Button(action: {
-            onLogout()
-        }, label: {
-            VStack {
-                AppImages.logOut
-                    .resizable()
-                    .renderingMode(.template)
-                    .frame(width: Constants.iconSize, height: Constants.iconSize)
-                    .foregroundColor(AppColors.accentMedium)
-            }
-            .frame(width: Constants.tabSize, height: Constants.tabSize)
-        })
+    var logoutButton: some View {
+        Button(action: onLogout) {
+            AppImages.logOut
+                .resizable()
+                .renderingMode(.template)
+                .frame(width: Constants.iconSize, height: Constants.iconSize)
+                .foregroundColor(Constants.unselectedTabColor)
+                .frame(width: Constants.tabSize, height: Constants.tabSize)
+        }
     }
 }
 
