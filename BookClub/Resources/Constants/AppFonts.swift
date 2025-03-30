24366c7fd701f39c61873500a44db6fe6ea286c0
TarkWight <arsentiy.gonda@gmail.com>
2025-03-30 13:44:52 +0700

    Setup app fonts

---
+++ BookClub/Resources/Constants/AppFonts.swift
@@ -0,0 +1,20 @@
+//
+//  AppFonts.swift
+//  BookClub
+//
+//  Created by Tark Wight on 29.03.2025.
+//
+
+import SwiftUI
+
+enum AppFonts {
+    static let title = Font.custom("Alumni Sans Bold", size: 96)
+    static let header1 = Font.custom("Alumni Sans Bold", size: 48)
+    static let header2 = Font.custom("Alumni Sans Bold", size: 24)
+    static let header3 = Font.custom("Alumni Sans Bold", size: 14)
+    static let body = Font.custom("Vela Sans", size: 16)
+    static let bodySmall = Font.custom("Vela Sans", size: 14)
+    static let footnote = Font.custom("Vela Sans", size: 10)
+    static let text = Font.custom("Georgia", size: 14)
+    static let quote = Font.custom("Georgia-Italic", size: 16)
+}
