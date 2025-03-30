506637167bb6311cf9159315390970175d32c6be
TarkWight <arsentiy.gonda@gmail.com>
2025-03-30 14:54:59 +0700

    Update font style view modifier
    -- now the view modifier is as custom as possible. Сan use both pre-made styles and customize parts of them, up to a fully custom style

--- BookClub/Resources/Constants/AppFonts.swift
+++ BookClub/Resources/Constants/AppFonts.swift
@@ -8,13 +8,13 @@
 import SwiftUI
 
 enum AppFonts {
-    static let title = Font.custom("Alumni Sans Bold", size: 96)
-    static let header1 = Font.custom("Alumni Sans Bold", size: 48)
-    static let header2 = Font.custom("Alumni Sans Bold", size: 24)
-    static let header3 = Font.custom("Alumni Sans Bold", size: 14)
-    static let body = Font.custom("Vela Sans", size: 16)
-    static let bodySmall = Font.custom("Vela Sans", size: 14)
-    static let footnote = Font.custom("Vela Sans", size: 10)
-    static let text = Font.custom("Georgia", size: 14)
-    static let quote = Font.custom("Georgia-Italic", size: 16)
+    static let title: String = "Alumni Sans Bold"
+    static let header1: String = "Alumni Sans Bold"
+    static let header2: String = "Alumni Sans Bold"
+    static let header3: String = "Alumni Sans Bold"
+    static let body: String = "Vela Sans"
+    static let bodySmall: String = "Vela Sans"
+    static let footnote: String = "Vela Sans"
+    static let text: String = "Georgia"
+    static let quote: String = "Georgia-Italic"
 }
