0ef8fe9d2501f0b89a07d28ba85dc2deffa6716b
TarkWight <arsentiy.gonda@gmail.com>
2025-05-29 15:22:59 +0700

    Added app colors enum

---
+++ BookClub/Resources/Constants/AppColors.swift
@@ -0,0 +1,18 @@
+//
+//  AppColors.swift
+//  BookClub
+//
+//  Created by Tark Wight on 25.03.2025.
+//
+
+import SwiftUI
+
+enum AppColors {
+    static let accentDark = Color("Accent Dark")
+    static let accentMedium = Color("Accent Medium")
+    static let accentLight = Color("Accent Light")
+    static let secondary = Color("AppSecondary")
+    static let background = Color("AppBackground")
+    static let black = Color("AppBlack")
+    static let white = Color("AppWhite")
+}
