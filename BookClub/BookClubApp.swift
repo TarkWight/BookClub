0ea0d8f943e675f7725177e67cc099c889fe2277
TarkWight <arsentiy.gonda@gmail.com>
2025-05-29 13:58:41 +0700

    Initial empty project
    -- SwiftUI
    -- CodeData
    -- XCTests

---
+++ BookClub/BookClubApp.swift
@@ -0,0 +1,18 @@
+//
+//  BookClubApp.swift
+//  BookClub
+//
+//  Created by Tark Wight on 29.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct BookClubApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookClub/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  BookClub
+//
+//  Created by Tark Wight on 29.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Select an item")
+    }
+}
+
+  
+#Preview {
+    ContentView()
+}
