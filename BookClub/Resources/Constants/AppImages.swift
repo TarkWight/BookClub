ce336f54da276ef70f62868fda5be5a59de9f2b5
TarkWight <arsentiy.gonda@gmail.com>
2025-05-29 15:34:54 +0700

    Added app images enum

---
+++ BookClub/Resources/Constants/AppImages.swift
@@ -0,0 +1,30 @@
+//
+//  AppImages.swift
+//  BookClub
+//
+//  Created by Tark Wight on 25.03.2025.
+//
+
+import SwiftUI
+
+enum AppImages {
+    static let eyeOn = Image("Eye On")
+    static let eyeOff = Image("Eye Off")
+    static let close = Image("Close")
+    static let library = Image("Library")
+    static let search = Image("Search")
+    static let bookmarks = Image("Bookmarks")
+    static let arrowLeft = Image("Arrow Left")
+    static let contents = Image("Contents")
+    static let play = Image("Play")
+    static let pause = Image("Pause")
+    static let settings = Image("Settings")
+    static let previous = Image("Previous")
+    static let next = Image("Next")
+    static let history = Image("History")
+    static let logOut = Image("Log Out")
+    static let read = Image("Read")
+    static let readingNow = Image("Reading Now")
+    static let decrement = Image("Decrement")
+    static let increment = Image("Increment")
+}
