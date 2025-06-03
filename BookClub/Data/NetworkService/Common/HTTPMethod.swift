c17bfd23165e4f4963591d10381daf68abc09c94
TarkWight <arsentiy.gonda@gmail.com>
2025-06-03 13:19:22 +0700

    Added http methods enum

---
+++ BookClub/Data/NetworkService/Common/HTTPMethod.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPMethod.swift
+//  BookClub
+//
+//  Created by Tark Wight on 01.06.2025.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case post = "POST"
+    case get = "GET"
+    case delete = "DELETE"
+    case put = "PUT"
+}
