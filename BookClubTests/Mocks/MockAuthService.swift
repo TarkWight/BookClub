18150910bf6ba91232b0f7134ce861e79976cf0b
TarkWight <arsentiy.gonda@gmail.com>
2025-06-04 17:23:20 +0700

    [Tests] Added mock auth network service

---
+++ BookClubTests/Mocks/MockAuthService.swift
@@ -0,0 +1,26 @@
+//
+//  MockAuthService.swift
+//  BookClub
+//
+//  Created by Tark Wight on 01.06.2025.
+//
+
+import Foundation
+@testable import BookClub
+
+@MainActor
+final class MockAuthService: AuthServiceProtocol {
+    var shouldSucceed = true
+    private(set) var refreshTokenCalled = false
+    private(set) var refreshTokenCallCount = 0
+
+    func refreshToken() async throws -> String {
+        refreshTokenCallCount += 1
+        refreshTokenCalled = true
+        if shouldSucceed {
+            return "new-token"
+        } else {
+            throw URLError(.notConnectedToInternet)
+        }
+    }
+}
