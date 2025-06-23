5b6258608cc5e890c0576c446f7717f13e18ad4b
TarkWight <arsentiy.gonda@gmail.com>
2025-06-23 21:51:45 +0700

    Adds Loadable enum for state management
    Introduces a `Loadable` enum to represent different states of data loading, such as idle, loading, loaded, and failure.
    
    This enum will be used throughout the application to manage the UI and handle data loading states consistently.

---
+++ BookClub/Features/Common/Helpers/Loadable.swift
@@ -0,0 +1,15 @@
+//
+//  Loadable.swift
+//  BookClub
+//
+//  Created by Tark Wight on 23.06.2025.
+//
+
+import Foundation
+
+enum Loadable<Value: Equatable>: Equatable {
+    case idle
+    case loading
+    case loaded(Value)
+    case failure(String)
+}
