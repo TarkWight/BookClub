105f626e133f0c09259e52e24861a070c08e0da3
TarkWight <arsentiy.gonda@gmail.com>
2025-06-23 21:52:57 +0700

    Defines search action enum
    Introduces a SearchAction enum to encapsulate all possible actions related to the search feature.
    
    This includes actions for:
    - Fetching data from local storage (genres, authors, recent searches)
    - Making network requests for books, authors, and genres
    - Handling user interactions (selecting author/genre, clearing search query, tapping search)
    - Selecting a specific book

--- BookClub/Features/Scenes/Search/Store/SearchAction.swift
+++ BookClub/Features/Scenes/Search/Store/SearchAction.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+
+enum SearchAction: Equatable {
+    // MARK: – Local storage (DB)
+    case fetchLocalGenres
+    case localGenresLoaded([GenreItem])
+
+    case fetchLocalAuthors
+    case localAuthorsLoaded([AuthorItem])
+
+    case fetchRecentSearches
+    case RecentSearchesLoaded([RecentSearches])
+
+    // MARK: – Network (API)
+	case requestFindBooksByName(query: String)
+    case findBooksByNameResponse(Result<[BookDetailsItem], NetworkError>)
+
+    case requestFindBooksByGenre(genreID: String)
+    case findBooksByGenreResponse(Result<[BookDetailsItem], NetworkError>)
+    
+    case requestFindBooksByAuthor(authorID: String)
+    case findBooksByAuthorResponse(Result<[BookDetailsItem], NetworkError>)
+
+    case requestGetAuthors
+    case findAuthorsResponse(Result<[AuthorItem], NetworkError>)
+
+    case requestGetGenres
+    case findGenresResponse(Result<[GenreItem], NetworkError>)
+
+    // MARK: - Part 1 selected
+    case dedSelectAuthor(authorID: String)
+    case dedSelectGenre(genreID: String)
+    case didSelectSearchQuery(query: String)
+    case didSearchQueryClear
+    case didSearchTapped(query: String)
+
+    // MARK: – Part 2 selected
+    case didSelectBook(documentID: String)
+}
