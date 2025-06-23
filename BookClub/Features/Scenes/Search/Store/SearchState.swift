c2b80ec700ec8ccdff21b39f767dd7445b576405
TarkWight <arsentiy.gonda@gmail.com>
2025-06-23 21:52:38 +0700

    Adds search state management
    Defines the data structures required to manage the state of the search feature.
    
    Introduces `SearchState`, `GenreItem`, `AuthorItem`, and `RecentSearches` structs to hold the search-related information.
    This enables the app to store and update states for genres, authors, recent searches, and selected items.

--- BookClub/Features/Scenes/Search/Store/SearchState.swift
+++ BookClub/Features/Scenes/Search/Store/SearchState.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+struct SearchState: Equatable, Sendable {
+    var genres: Loadable<GenreItem> = .idle
+    var authors: Loadable<AuthorItem> = .idle
+    var recentSearches: Loadable<RecentSearches> = .idle
+    
+    var selectedGenre: String?
+    var selectedAuthor: String?
+    var selectedRecentSearch: String?
+    
+    var selectedBookID: String?
+}
+
+struct GenreItem: Identifiable, Equatable, Codable {
+    var id: Int64
+    var name: String
+}
+
+struct AuthorItem: Identifiable, Equatable, Codable {
+    var id: Int64
+    var name: String
+    var imageUrl: String?
+}
+
+struct RecentSearches: Identifiable, Equatable, Codable {
+    var id: Int64
+    var name: String
+}
