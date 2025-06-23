b611f7c9eefa93970a4f870d6d19013374fcc015
TarkWight <arsentiy.gonda@gmail.com>
2025-06-23 14:06:12 +0700

    Adds Book entity to Core Data
    Introduces a Core Data entity `BookEntity` to persist book data.
    This includes the entity definition in the data model and the
    corresponding Swift class with properties mirroring the `Book` domain
    model. It also adds methods to convert between the Core Data entity
    and the domain model.

---
+++ BookClub/Domen/Book.swift
@@ -0,0 +1,20 @@
+//
+//  Book.swift
+//  BookClub
+//
+//  Created by Tark Wight on 23.06.2025.
+//
+
+import Foundation
+
+struct Book: Equatable, Sendable {
+    let id: Int64
+    let documentId: String
+    let title: String
+    let coverURL: URL?
+    let illustrationURL: URL?
+    let isNew: Bool
+    let createdAt: Date
+    let publishedAt: Date
+    let updatedAt: Date
+}
