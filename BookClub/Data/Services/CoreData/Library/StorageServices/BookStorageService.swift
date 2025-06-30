ce3323cf8d3e5e1bfc5a1c8520b04bdc94db9d1e
TarkWight <arsentiy.gonda@gmail.com>
2025-06-30 11:47:25 +0700

    Refactors BookStorageService for concurrency
    Removes the actor keyword from BookStorageService, as it was causing issues with Core Data concurrency.
    
    Updates deleteAll function to fetch and delete books individually to avoid issues with batch deletion in Core Data.

--- BookClub/Data/Services/CoreData/Library/StorageServices/BookStorageService.swift
+++ BookClub/Data/Services/CoreData/Library/StorageServices/BookStorageService.swift
@@ -7,7 +7,7 @@
 
 import CoreData
 
-actor BookStorageService: BookStorageServiceProtocol {
+final class BookStorageService: BookStorageServiceProtocol {
 
     private let container: NSPersistentContainer
 
@@ -66,25 +66,30 @@ actor BookStorageService: BookStorageServiceProtocol {
 
     func deleteAll() async throws {
         try await container.performBackgroundTask { context in
-            let fetch: NSFetchRequest<NSFetchRequestResult> =
-                BookEntity.fetchRequest()
-            let delete = NSBatchDeleteRequest(fetchRequest: fetch)
-            try context.execute(delete)
-            try context.save()
+
+            let fetchReq: NSFetchRequest<BookEntity> = BookEntity.fetchRequest()
+            let allBooks = try context.fetch(fetchReq)
+
+            for book in allBooks {
+                context.delete(book)
+            }
+            if context.hasChanges {
+                try context.save()
+            }
         }
     }
 
-  func setFavorite(
-    documentId: String,
-    to isFav: Bool
-  ) async throws {
-    try await container.performBackgroundTask { ctx in
-      let req: NSFetchRequest<BookEntity> = BookEntity.fetchRequest()
-      req.predicate = NSPredicate(format: "documentId == %@", documentId)
-      if let book = try ctx.fetch(req).first {
-        book.isFavorite = isFav
-        try ctx.save()
-      }
+    func setFavorite(
+        documentId: String,
+        to isFav: Bool
+    ) async throws {
+        try await container.performBackgroundTask { ctx in
+            let req: NSFetchRequest<BookEntity> = BookEntity.fetchRequest()
+            req.predicate = NSPredicate(format: "documentId == %@", documentId)
+            if let book = try ctx.fetch(req).first {
+                book.isFavorite = isFav
+                try ctx.save()
+            }
+        }
     }
-  }
 }
